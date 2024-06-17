7be500a07c7c6f61fd3cd902e93085935fef060f
Emmy123222 <emmanuelogheneovo17@gmail.com>
2024-06-17 14:56:14 +0100

    if else statement in javascript

---
+++ day2.js
@@ -0,0 +1,94 @@
+// let mood = 'sleepy'
+// let tirednessLevel = 12;
+
+// if (mood === 'sleepy' && tirednessLevel < 8) {
+//     console.log('Time to sleep')
+// } else {
+//     console.log('Not time to sleep')
+// }
+
+
+// for (let i = 0; i < 6; i++) {
+//     console.log(i)
+// }
+
+// function myNumber(a , b) {
+// return a + b
+// }
+
+// const myBuilding = myNumber(7 , 8)
+// console.log(myBuilding)
+
+
+// let musician = ['davido', 'wizkid', 'olamide' , 'youngjohn'];
+
+// for (let i = 0; i < musician.length; i++) {
+// console.log('i will love to sing with' + ' ' + musician[i])
+// }
+
+
+// let family = ['father', 'mother', ['manone', 'mantwo', 'manthree', ['james', 'abel', 'caleb']],
+// ['girlone', 'girltwo', 'girlthree', ['sarah', 'grace', 'favour']]];
+
+
+// console.log(family[2][3][0])
+// console.log(family[3][3][1])
+
+// let arr = [1,2,3,4,5,6,7,89,12,32,45,65,43,12]
+
+// function myNumber(number) {
+//     let newNum = number.filter(number => number > 10)
+//     return newNum
+// }
+
+// console.log(myNumber(arr))
+
+
+// let friut = ['banana','apple','mango','pineapple'];
+
+// console.log(friut[0])
+
+// friut[1] = 'carrot'
+// console.log(friut)
+
+// friut.push('star friut','cucumber')
+// console.log(friut)
+
+// friut.pop('mango','carrot');
+// console.log(friut);
+
+// let mood = 'sleepy';
+// let tirednessLevel = 20;
+
+// if (mood === 'sleepy' && tirednessLevel < 10) {
+//     console.log('Time to sleep')
+// } else {
+//     console.log('Not time to sleep')
+// }
+
+
+// let season = 'summer';
+
+// if (season === 'winter') {
+//     console.log('it is raining')
+// } else if (season === 'summer') {
+//     console.log('it is sunny')
+// } else if (season === 'spring') {
+//     console.log('Tree are falling')
+// } else {
+//     console.log('not raining')
+// }
+
+let musician = '';
+
+if (musician === 'jurld world') {
+    console.log('he is just a singer not a rapper')
+} else if (musician === 'polo g') {
+    console.log('he is also a singer not a rapper')
+} else if (musician === 'adele') {
+    console.log('she is a wonderful singer')
+} else {
+    console.log('all are just rapper and not a singer')
+}
+
+
