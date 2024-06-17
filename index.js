fbc16349fd36137f5f577c021575fc7beac5e9c9
Emmy123222 <emmanuelogheneovo17@gmail.com>
2024-06-17 15:37:12 +0100

    function

---
+++ index.js
@@ -0,0 +1,508 @@
+//let isNightTime = true;
+
+//if (isNightTime) {
+  //console.log('Turn on the lights!');
+//} else {
+//  console.log('Turn off the lights!');
+//}
+
+//isNightTime ? console.log('Turn on the light!') : console.log('Turn off the light!');
+
+//let mood = 'sleepy';
+
+//if (mood) {
+  //console.log('Time to sleep');
+//} else {
+ // console.log('Not time to sleep yet');
+//}
+
+//mood ? console.log('Time to sleep') : console.log('Not time to sleep');
+
+//let music = 'dance';
+
+//if (music) {
+  //console.log('Time to dance');
+//} else {
+ // console.log('I am not dancing');
+//}
+
+//music ? console.log('Time to dance') : console.log('I am not dancing
+
+
+
+
+//function areaOfRectangle(width, height){
+    // return width * height
+//}
+
+//const area1 = areaOfRectangle(7, 4);
+//const area2 = areaOfRectangle(8, 4);
+//const area3 = areaOfRectangle(4, 2);
+//console.log(area1)
+
+//function gameOver() {
+  //console.log('Time to play video games.');
+//}
+
+//function sayThanks() {
+  //console.log('Thank you for your purchase! We appreciate your business.')
+//}
+//sayThanks()
+//sayThanks()
+//sayThanks()
+
+//function sayThanks(name) {
+  //console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+//}
+//sayThanks('Nico')
+
+//function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+ // console.log(`Remember to buy ${item1}`);
+  //console.log(`Remember to buy ${item2}`);
+  //console.log(`Remember to buy ${item3}`);
+//}
+
+//function monitorCount(rows , columns) {
+  //return rows * columns
+//};
+//const numOfMonitors = monitorCount(5 , 4)
+//console.log(numOfMonitors);
+
+//function rectangleArea(width, height) {
+ // let area = width * height;
+//}
+//console.log(rectangleArea(5, 7)) // Prints undefined
+
+//function campNumbers(counts , numbered) {
+ // return counts * numbered
+//};
+//const numOfHouses = campNumbers(6 , 6)
+  //console.log(numOfHouses);
+
+  //let mood = 'sleepy';
+  //let tirednessLevel = 6;
+
+  //if (mood === 'sleepy' && tirednessLevel > 8) {
+    //console.log('Time to sleep')
+  //} else {
+    //console.log('Not time to sleep')
+  
+
+    //let saloon = ['wigs','haircream','haircomb'];
+////console.log(saloon[2]);
+
+//saloon[0] = 'cream';
+//console.log(saloon);
+
+//saloon[2] = 'milk';
+//console.log(saloon);
+
+//saloon[0] = 'icecream';
+//console.log(saloon);
+
+//for (let i = 0; i <= 10; i++){
+  ///console.log(i);
+//}
+
+// for (let music = 4; music <= 11; music++){
+//   console.log(music);
+// }
+
+
+//const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+//for (let i = 0; i < vacationSpots.length; i++ ){
+  //console.log('I would love to visit ' + vacationSpots[i]);
+//}
+
+// console.log(vacationSpots[0])
+
+// const myArray = [6, 19, 20];
+// const yourArray = [19, 81, 2];
+// for (let i = 0; i < myArray.length; i++) {
+//   for (let j = 0; j < yourArray.length; j++) {
+//     if (myArray[i] === yourArray[j]) {
+//       console.log('Both arrays have the number: ' + yourArray[j]);
+//    
+ //}
+//   }
+// }
+
+//function myHouses(music , dance) {
+//return music * dance
+//};
+//const numOfHouses = myHouses(6 , 6);
+//console.log(numOfHouses);
+
+// A for loop that prints 1, 2, and 3
+
+
+//for (let music = 1; music < 4; music++){
+ // console.log(music);
+//}
+
+//let music = 1;
+//while (music < 4);
+//console.log(music);
+//music++;
+
+//let song = ['Hiphop' , 'Gospel' , 'Gala'];
+//console.log(song[0]);
+
+//song[0] = 'dancing'
+//console.log(song)
+
+//song[1] = 'singing'
+//console.log(song);
+
+//function myHouses(dancing , singing) {
+ // return dancing * singing
+//}
+
+//const myBuilding = myHouses(6 , 6) 
+ // console.log(myBuilding);
+
+ // moody = 'sleepy';
+ //let tirednessLevel = 6;
+
+ //if (moody === sleepy && tirednessLevel < 4) {
+ // console.log('Time to sleep');
+ //} else {
+ // console.log('Not time to sleep');
+ //}
+
+ ////function myHouses(a , b) {
+  //return a + b
+ //}
+
+ //const myBuilding = myHouses(10 , 10) 
+  //console.log(myBuilding);
+ 
+
+//   let music = 'song';
+// let hiphop = 2;
+
+//   if (music === 'song' || hiphop > 10) {
+//     console.log('Time to dance');
+//   } else {
+//     console.log('Not time to dance')
+//   }
+
+
+// let countString = '';
+// let i = 0;
+
+// do {
+//   countString = countString + i;
+//   i++;
+// } while (i < 5);
+
+//for (let i = 0; i < 10; i++) {
+ // console.log(i)
+//}
+
+//const saloon = ['cream' , 'relaxer' , 'hair cream' , 'comb']
+//console.log(saloon[0])
+
+//saloon[0] = 'hair comb'
+//console.log(saloon)
+
+//isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+//let isNightTime = true;
+
+
+
+//for (let i = 0; i < 100; i++){
+//console.log(i)
+//}
+
+//let music = 'song';
+//let rappers = 6;
+
+//if (music === 'song' && rappers < 10) {
+  //console.log('Time to sleep')
+//} else {
+ // console.log('Not time to sleep')
+//}
+
+//const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+//for(let i = 0; i < rapperArray.length; i++){
+  //if(rapperArray[i] === 'Notorious B.I.G.'){
+    // console.log(rapperArray[i])
+    //break;
+  //}
+   //console.log(rapperArray[i])
+//}
+//console.log("And if you don't know, now you know.")
+
+
+
+// const friuts = ['mango', 'papaya', 'pineapple', 'apple'];
+
+
+
+// const word = 'names of your friends';
+// const splitWord = word.split(' ');
+ 
+// const letters = splitWord.map((words) => words[0]);
+// letters.join('\n');
+
+// console.log(letters.forEach((add) => {
+//   return add === 26
+// }))
+ 
+
+// const animals = ['Emmy', 'Daniel', 'Ruth', 'David', 'Micheal', 'joyce', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// // Create the secretMessage array below
+// const secretMessage = animals.map((animals) => {
+//   return animals[0]
+// })
+
+// console.log(secretMessage);
+// console.log(secretMessage.join(' '));
+
+// const bigNumbers = [200, 300, 400, 500, 600];
+
+// // Create the smallNumbers array below
+// const smallNumbers = bigNumbers.map((num) => {
+//   return num / 100
+// })
+// console.log(smallNumbers)
+
+
+
+// const array = [10, 20, 30, 40];
+
+// const newArray = array.forEach(())
+
+// 
+
+// const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// // Iterate over fruits below
+// fruits.forEach(fruit => console.log(`I want to eat a ${fruit}.`))
+
+// 
+
+// const numbers = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ,]
+
+// const evenNumbers = numbers.filter((number) => {
+// return number % 3 !== 0
+// });
+
+// console.log(evenNumbers);
+
+// 
+
+// 
+
+//  const spaceship = {
+//   telescope: {
+//     yearBuilt: 2018,
+//     model: '91031-XLT',
+//     focalLength: 2032 
+//  },
+// crew: {
+//     captain: { 
+//         name: 'Sandra', 
+//         degree: 'Computer Engineering', 
+//         encourageTeam() { console.log('We got this!') } 
+//      }
+// },
+// engine: {
+//     model: 'Nimbus2000'
+//  },
+//  nanoelectronics: {
+//      computer: {
+//         terabytes: 100,
+//         monitors: 'HD'
+//      },
+//     'back-up': {
+//        battery: 'Lithium',
+//        terabytes: 50
+//      }
+// }
+// }; 
+
+// console.log(spaceship);
+
+// for (let i = 0; i <= 5; i++) {
+//   console.log(i);
+// }
+
+
+//  let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ,12]
+//  let newNumber = [];
+//  for (let i = 0; i < number.length; i++) {
+//   if (number[i] < 10) {
+//   newNumber.push(number[i])
+//   }
+//  }
+
+//  console.log(newNumber);
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+let newArr = [0,34,3,3,43,3,4,34,23,2]
+
+function myNumbers(myArr) {
+  let number = [];
+  for (let i = 0; i < myArr.length; i++) {
+    if (myArr[i] < 10) {
+      number.push(myArr[i]);
+    }
+  }
+  return number
+}
+
+myNumbers(arr)
+let a = [3,5,99,65]
+console.log(myNumbers(arr));
+console.log("This is a new line..............");
+console.log(myNumbers(newArr));
+
+
+// function add(a, b){
+//   console.log(a + b)
+// }
+
+// add(3, 7)
+
+// 
+
+// for (let i = 0; i < 10; i++) {
+//   if (i === 8) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// let i = 0;
+// while (i < 10) {
+//   if (i === 8) {
+//     break;
+//   }
+//   console.log(i)
+//   i++
+// }
+
+// let obj = {a: 1, b: 2, c: 3};
+// for (let key in obj) {
+//   if (key === 'b') {
+//     break;
+//   }
+//   console.log(obj[key]);
+// }
+
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let element of arr) {
+//   if (element === 5) {
+//     break;
+//   }
+//   console.log(element);
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   }
+//   console.log(i)
+// }
+
+// let i = 0;
+// while (i < 10) {
+//   i++;
+//   if (i % 2 === 0) {
+//     continue;
+//   }
+//   console.log(i)
+// }
+
+// let i = 0;
+// do {
+//   i++;
+//   if (i % 2 ===0) {
+//     continue;
+//   }
+//   console.log(i)
+// } while (i <10);
+
+// 
+
+// let obj = {a: 1, b: 2, c: 3, d: 4};
+// for (let key in obj) {
+//   if (obj[key] % 2 === 0) {
+//     continue;
+//   }
+//   console.log(obj[key])
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let element of arr) {
+//   if (element % 2 === 0) {
+//     continue;
+//   }
+//   console.log(element)
+// }
+
+// function maxNumbers(num1, num2) {
+//   return num1 >= num1 ? num1 : num2;
+// }
+
+// console.log(maxNumbers(2,1));
+
+
+// function fizzBuzz(number) {
+//   if (number % 3 === 0 && number % 5 === 0) 
+//     return 'fizzBuzz';
+//   else if (number % 3 === 0)
+//   return 'fizz';
+//   else if (number % 5 === 0)
+//     return 'Buzz'
+//   else 
+//   return number;
+// }
+
+// console.log(fizzBuzz(9));
+
+// function displayEvenNumbers(array) {
+//   for (const number of  array) {
+//     if (number % 2 === 0)
+//       console.log(number);
+//   }
+// }
+
+// displayEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+// function displayEvenNumbers(array) {
+//   for (const number of  array) {
+//     if (number % 2 != 0)
+//       console.log(number);
+//   }
+// }
+
+// displayEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+// let spaceship = {
+//   'Fuel Type' : 'Turbo Fuel',
+//   'Active Mission' : true,
+//   homePlanet : 'Earth', 
+//   numCrew: 5
+//  };
+
+// let propName =  'Active Mission';
+
+let dog = {
+  name: "Buddy",
+  breed: "Golden Retriever",
+  age: 3,
+}
+   
+console.log(dog); 
+console.log(dog.name);
+
+dog.name = 'Emma'
+console.log(dog.name)
